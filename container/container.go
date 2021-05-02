78893d5459b58170a0cb3357a3b634e21374ef48
Nich Frai <Nonanick@users.noreply.github.com>
2021-05-02 11:35:27 -0300

    reorganization of folder struct

--- container/container.go
+++ container/container.go
@@ -1,4 +1,21 @@
 package container
 
+import (
+	"github.com/Nonanick/go-maestro/controller"
+	"github.com/Nonanick/go-maestro/proxy"
+	"github.com/Nonanick/go-maestro/server/routes"
+)
+
 type Container struct {
+	routes.Provider
+
+	Name    string
+	BaseURL string
+
+	Controllers []controller.Controller
+
+	RequestProxies  []proxy.Request
+	ResponseProxies []proxy.Response
+
+	Validations []
 }
